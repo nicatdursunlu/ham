33256b48b0c9a850d47f1667740f384c8eb0afff
Nicat DUrsunlu <nicat.dursunlu@gmail.com>
2020-10-02 18:38:25 +0400

    fix bugs

--- js/script.js
+++ js/script.js
@@ -15,50 +15,18 @@ designLi.click(function () {
   $(`.design-text[dataValue="${match1}"]`).css("display", "flex");
 });
 
+
 // our amazing work
-
-// let loadMore = $(".load-more");
-// let amazingImages = $(".amazing-work-images-item");
-// let amazingWork = $(".amazing-work");
-
-// let x = 12;
-// amazingImages.slice(0, x).show();
-
-// amazingWork.on("click", (event) => {
-//   if ($(event.target).attr("btn") == "btn") {
-//     x += 12;
-//     amazingImages.slice(0, x).show();
-//     if (x == 36) {
-//       loadMore.hide();
-//     }
-//   } else if (event.target.tagName == "LI") {
-//     let dataKey = $(event.target).attr("datakey");
-//     if (x > 12 && !dataKey) {
-//       if (x !== 36) {
-//         loadMore.css("display", "flex");
-//       }
-//       return false;
-//     }
-//     amazingImages.hide();
-//     loadMore.hide();
-//     $(".option-style").removeClass("option-style");
-//     $(event.target).addClass("option-style");
-//     x = 12;
-//     if (dataKey) {
-//       $(`.${dataKey}`).show();
-//     } else {
-//       amazingImages.slice(0, x).show();
-//       loadMore.css("display", "flex");
-//     }
-//   }
-// });
 
 let loadMore = $(".load-more");
 let amazingImages = $(".amazing-work-images-item");
 let amazingWork = $(".amazing-work");
+
 let x = 12;
 amazingImages.slice(0, x).show();
+
 amazingWork.on("click", (event) => {
+  
   if ($(event.target).attr("btn") == "btn") {
     x += 12;
     amazingImages.slice(0, x).show();
@@ -66,6 +34,7 @@ amazingWork.on("click", (event) => {
       loadMore.hide();
     }
   } else if (event.target.tagName == "LI") {
+
     let dataKey = $(event.target).attr("datakey");
     if (x > 12 && !dataKey) {
       if (x !== 36) {
@@ -73,11 +42,14 @@ amazingWork.on("click", (event) => {
       }
       return false;
     }
+
     amazingImages.hide();
     loadMore.hide();
+
     $(".option-style").removeClass("option-style");
     $(event.target).addClass("option-style");
     x = 12;
+
     if (dataKey) {
       $(`.${dataKey}`).show();
     } else {
